959f32fe8dcf98d377804491586f0f279c9209fc
Md. Sabbir Hossain <78879656+isfaq-hossain@users.noreply.github.com>
2023-09-16 11:03:23 +0600

    Update binarySearch.c

--- binarySearch.c
+++ binarySearch.c
@@ -19,10 +19,30 @@ int binarySearch(int arr[], int key, int start, int end)
 
 }
 
+
+void Sort(int a[], int n)
+{
+    int i, j, temp;
+    for (i = 0; i < n-1; i++)
+    {
+        for (j = 0; j < n -1 - i; j++)
+        {
+            if (a[j] > a[j+1])
+            {
+                temp = a[j+1];
+                a[j+1] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
+
+
 int main()
 {
-    int arr[] = { 2, 3, 4, 10};
+    int arr[] = {202,104,304,223,453,123};
     int n = sizeof(arr) / sizeof(arr[0]);
+    Sort(arr, n);
     printf("Enter a key - ");
     int key;
     scanf("%d",&key);
@@ -31,6 +51,11 @@ int main()
         printf("Element not found\n");
     }
     else{
-        printf("Element found at index - %d", result);
+        printf("Elements in the array are - ");
+        for(int i = 0; i < n; i++)
+        {
+           printf("%d ",arr[i]);
+        }
+        printf("\nElement found at index - %d", result);
     }
 }
